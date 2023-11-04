f46f1b0e6702021751bd8df9517727a09b17a795
Parth Ahuja <parthahuja2003@gmail.com>
2023-11-04 16:52:28 +0530

    modify app.js

--- server/app.js
+++ server/app.js
@@ -1,10 +1,15 @@
 import express from "express"
-import {PORT,mongoDBURL} from "./config.js"
 import mongoose from "mongoose"
 import userRoutes from './routes/userRoutes.js'
 import ngoRoutes from './routes/ngoRoutes.js'
 import cors from 'cors'
+import { PORT } from "./config.js"
 import ExpressError from "./utils/ExpressError.js"
+import 'dotenv/config'
+import session from "express-session"
+import MongoStore from "connect-mongo"
+import passport from "passport"
+
 const app=express();
 //middleware for parsing req body in json
 app.use(express.json());
@@ -18,6 +23,36 @@ app.use(cors({
     allowedHeaders:['Content-Type'],
 }));
 
+const mongoDBURL=process.env.DB_URL;
+//session
+const secret = process.env.SECRET;
+
+const store = MongoStore.create({
+  mongoUrl: mongoDBURL,
+  touchAfter: 24 * 60 * 60,
+  crypto: {
+      secret,
+  }
+});
+
+store.on("error", function (e) {
+    console.log("SESSION STORE ERROR", e)
+})
+
+const sessionConfig = {
+  store,
+  name: "session",
+  secret,
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    httpOnly: true,
+    //secure:true,
+    expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+  },
+};
+app.use(session(sessionConfig));
 
 app.get("/",(req,res)=>{
     res.send("Hello Bhai, welcome to the home page");
@@ -26,7 +61,7 @@ app.all('*',(req,res,next)=>{
     next(new ExpressError("Page Not found",404))
 })
 app.use((err,req,res,next)=>{
-    const {statusCode=500,message}=err;
+    const {statusCode=500,message}=err; 
     if(!err.message)err.message='Something went wrong'
     res.status(statusCode).send(message);
 })
